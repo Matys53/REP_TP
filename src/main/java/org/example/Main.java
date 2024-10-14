049e6a2093f4f2928451e7f3078ae4e3ea0a54d0
matys <matys.courousse@insa-rennes.fr>
2024-10-14 11:21:22 +0200

    commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int iterations = 1000000;
+        int validCount = 0;
+
+        Random random = new Random();
+
+        for (int i = 0; i < iterations; i++) {
+            float x = random.nextFloat() * 2e3f - 1e3f;
+            float y = random.nextFloat() * 2e3f - 1e3f;
+            float z = random.nextFloat() * 2e3f - 1e3f;
+
+            float leftSide = (x + y) + z;
+            float rightSide = x + (y + z);
+
+            if (leftSide - rightSide == 0) {
+                validCount++;
+            }
+        }
+
+        double percentageValid = (validCount * 100.0) / iterations;
+        System.out.println("Pourcentage d'égalités valides : " + percentageValid + "% ");
+    }
+
+}
