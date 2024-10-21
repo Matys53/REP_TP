31841de20867161f9c4f31a07b1839e6a653c94a
matys <matys.courousse@insa-rennes.fr>
2024-10-21 08:46:31 +0200

    correct error

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,6 +10,7 @@ public class Main {
 
         Random random = new Random();
 
+        // Range : -1000 ; 1000
         for (int i = 0; i < iterations; i++) {
             float x = random.nextFloat() * 2e3f - 1e3f;
             float y = random.nextFloat() * 2e3f - 1e3f;
